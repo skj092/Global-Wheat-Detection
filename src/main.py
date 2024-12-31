6a9e05a25a6d92d2d627db85a21dfa728bb08ebb
skj092 <sonujha092@gmail.com>
2024-12-31 18:53:51 +0530

    create yaml file with python code

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 from ultralytics import YOLO
+import yaml
+
+data = {
+    'train': '/home/sonujha/rnd/Wheat-Head-Detection-YOLO/data/preprocessed/images/train',
+    'val': '/home/sonujha/rnd/Wheat-Head-Detection-YOLO/data/preprocessed/images/valid',
+    'nc': 1,
+    'names': ['wheat']
+}
+
+with open('wheat.yaml', 'w') as f:
+    yaml.dump(data, f, default_flow_style=False)
 
 # Load a COCO-pretrained YOLOv8n model
 model = YOLO("yolov8n.pt")
 
 # Display model information (optional)
-model.info()
+print(model.info())
 
 # Train the model on the COCO8 example dataset for 100 epochs
 results = model.train(data="wheat.yaml", epochs=10, imgsz=640)
